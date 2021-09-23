74b2278c4ae7b09aed4055bc50b0c89b695a431f
Gordan Kranjčić <gordankranjcic@gmail.com>
2021-09-23 09:22:42 +0200

    MyPet

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Labrador",
+    name: "Jingle",
+    legs: 4,
+    friends: ["Sally", "Peter"]
+}
+function myFunction(obj){
+    return obj;
+} 
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = {myPet,myFunction};
